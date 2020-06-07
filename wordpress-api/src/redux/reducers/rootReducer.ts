00f1f7cc0173eb0e703fea46bd727b30ffd0e52a
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-07 22:22:50 +0200

    imported and added post reducer

--- wordpress-api/src/redux/reducers/rootReducer.ts
+++ wordpress-api/src/redux/reducers/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import exampleReducer from "./toolsReducer";
+import postReducer from "./postReducer";
 
 export const rootReducer = combineReducers({
 	example: exampleReducer,
+	posts: postReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
