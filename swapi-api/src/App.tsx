ded872740b28bdd55980dc71d572ac797309e3ea
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-11 19:26:16 +0200

    add toolbar component

--- swapi-api/src/App.tsx
+++ swapi-api/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "stores/store";
 import { Header, Content, Footer } from "components/+Layout";
+import { Toolbar } from "@material-ui/core";
 
 import "./App.css";
 
@@ -13,6 +13,8 @@ function App() {
 			<div className='App'>
 				<Router>
 					<Header />
+					<Toolbar />
+					<Toolbar />
 					<Content />
 					<Footer />
 				</Router>
