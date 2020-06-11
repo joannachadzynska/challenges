8fc7c3442a14b4b3dfde40351f41eebfa98bd1e1
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-11 22:09:53 +0200

    fetch data

--- swapi-api/src/components/+Layout/+Content/Content.tsx
+++ swapi-api/src/components/+Layout/+Content/Content.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home, NotFound } from 'pages';
+import { useDispatch } from 'react-redux';
+import { getAllCharacters } from 'redux/actions/charactersAction';
+import { NotFound } from 'pages';
+import { CustomizedTable as Table } from 'components/+Table/Table';
 
 export interface ContentProps {}
 
 const Content: React.SFC<ContentProps> = () => {
+  const dispatch = useDispatch();
+
+  const fetchData = useCallback(() => {
+    dispatch(getAllCharacters());
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <section>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Table />
         </Route>
         <Route>
           <NotFound />
