6cbca78f9bd77f3f9be0b3b3a41119ddb22b03b5
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-18 19:57:28 +0200

    learn types: number, string, boolean

--- drag-n-drop/app.ts
+++ drag-n-drop/app.ts
@@ -1,1 +1,15 @@
-console.log('Your code goes here...');
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+	const result = num2 + num2;
+	if (showResult) {
+		console.log(phrase + result);
+	} else {
+		return result;
+	}
+}
+
+const number1 = 5;
+const number2 = 3.4;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
