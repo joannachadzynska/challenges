4052f91cd963ca68575fce6e3bb30feeab206442
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-20 19:40:23 +0200

    basic compiler settings

---
+++ learn-TS/compiler-setup/analytics.ts
@@ -0,0 +1,1 @@
+console.log("Searching data...");
--- learn-TS/compiler-setup/app.ts
+++ learn-TS/compiler-setup/app.ts
@@ -1,22 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = "Joanna";
 
-userInput = 4;
-userInput = "hello";
-
-if (typeof userInput === "string") {
-	userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-	// whats return "never" type:
-	throw {
-		message: message,
-		errorCode: code,
-	};
-	// while (true) {}
-}
-
-const result = generateError("Some error occurred!", 500);
-
-console.log(result);
+console.log(userName);
