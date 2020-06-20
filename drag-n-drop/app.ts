e6909c1318536a975efd2e1bea6b59f15a2327bf
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-20 19:06:30 +0200

    function types and callbacks

--- drag-n-drop/app.ts
+++ drag-n-drop/app.ts
@@ -1,9 +1,14 @@
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number) {
 	return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printResult(num: number) {
 	console.log(`Result: ${num}`);
+}
+
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
+	const result = n1 + n2;
+	callback(result);
 }
 
 printResult(add(4, 5));
@@ -12,3 +17,7 @@ let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+	console.log(result);
+});
