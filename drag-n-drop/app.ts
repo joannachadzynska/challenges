a7b10497e5aaa0b2233a4f08411c51a6ba98579f
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-20 18:42:33 +0200

    learn type aliases with literal types

--- drag-n-drop/app.ts
+++ drag-n-drop/app.ts
@@ -1,8 +1,11 @@
 // Union Type
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-	input1: number | string,
-	input2: number | string,
-	resultConversion: "as-number" | "as-text"
+	input1: Combinable,
+	input2: Combinable,
+	resultConversion: ConversionDescriptor
 ) {
 	let result;
 	if (
