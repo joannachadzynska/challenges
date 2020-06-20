e42f2068e8edb5490aa6474a854765e9a66c1177
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-20 19:19:11 +0200

    never type

--- drag-n-drop/app.ts
+++ drag-n-drop/app.ts
@@ -7,3 +7,16 @@ userInput = "hello";
 if (typeof userInput === "string") {
 	userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+	// whats return "never" type:
+	throw {
+		message: message,
+		errorCode: code,
+	};
+	// while (true) {}
+}
+
+const result = generateError("Some error occurred!", 500);
+
+console.log(result);
