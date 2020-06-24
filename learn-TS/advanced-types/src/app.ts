e3fa4a8b1f502cd602b51c086fafb658074baa9d
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-24 19:36:46 +0200

    discriminated unions

--- learn-TS/advanced-types/src/app.ts
+++ learn-TS/advanced-types/src/app.ts
@@ -75,3 +75,31 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+// Discriminated unions
+interface Bird {
+	type: "bird";
+	flyingSpeed: number;
+}
+
+interface Horse {
+	type: "horse";
+	runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+	let speed;
+	switch (animal.type) {
+		case "bird":
+			speed = animal.flyingSpeed;
+			break;
+
+		case "horse":
+			speed = animal.runningSpeed;
+	}
+	console.log(`Moving with speed: ${speed}`);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 25 });
