a10dde93fdcb5142d1e1bbfbee5a879f69164d47
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-24 19:30:22 +0200

    Type Guards

--- learn-TS/advanced-types/src/app.ts
+++ learn-TS/advanced-types/src/app.ts
@@ -23,4 +23,55 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
-console.log(ee1);
+// Type Guards
+function add(a: Combinable, b: Combinable) {
+	if (typeof a === "string" || typeof b === "string") {
+		return a.toString(+b.toString());
+	}
+	return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+	console.log(`Name: ${emp.name}`);
+	if ("privileges" in emp) {
+		console.log(`Privileges: ${emp.privileges}`);
+	}
+	if ("startDate" in emp) {
+		console.log(`Start date: ${emp.startDate}`);
+	}
+}
+
+printEmployeeInformation(ee1);
+
+class Car {
+	drive() {
+		console.log("Driving...");
+	}
+}
+
+class Truck {
+	drive() {
+		console.log("Driving a truck...");
+	}
+
+	loadCargo(amount: number) {
+		console.log(`Loading cargo: ${amount}`);
+	}
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+	vehicle.drive();
+	if (vehicle instanceof Truck) {
+		vehicle.loadCargo(1000);
+	}
+}
+
+useVehicle(v1);
+useVehicle(v2);
