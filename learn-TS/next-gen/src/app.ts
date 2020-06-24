d4c1b6fd34c1e100f45094d65c3d9dd4a53977a7
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-24 19:07:56 +0200

    interfaces and classes

--- learn-TS/next-gen/src/app.ts
+++ learn-TS/next-gen/src/app.ts
@@ -1,12 +1,24 @@
-interface Greetable {
-	name: string;
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+	(a: number, b: number): number;
+}
 
+let add: AddFn;
+
+add = (n1: number, n2: number) => n1 + n2;
+
+interface Named {
+	readonly name?: string;
+	outputName?: string;
+}
+
+interface Greetable extends Named {
 	greet(phrase: string): void;
 }
 
 class Person implements Greetable {
 	age = 33;
-	constructor(public name: string) {}
+	constructor(public name?: string) {}
 
 	greet(phrase: string) {
 		console.log(`${phrase} ${this.name}`);
@@ -16,6 +28,7 @@ class Person implements Greetable {
 let user1: Greetable;
 
 user1 = new Person("Joanna");
+// user1.name = "John"
 
 user1.greet("Hi there - I am");
 
