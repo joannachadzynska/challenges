0267669af09ab2b7454b2600dd6da8053359493c
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-24 19:44:13 +0200

    Type Casting

--- learn-TS/advanced-types/src/app.ts
+++ learn-TS/advanced-types/src/app.ts
@@ -103,3 +103,15 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 25 });
+
+// Type Casting
+const paragraph = document.querySelector("p");
+const paragraphWithId = document.getElementById("message-output");
+// const userInputElement = <HTMLInputElement>(
+// 	document.getElementById("user-input")!
+// );
+const userInputElement = document.getElementById(
+	"user-input"
+)! as HTMLInputElement;
+
+userInputElement.value = "hi";
