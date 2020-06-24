5c138c84459c8ba88aaef75d9869cd201b978c3b
Joanna Chądzyńska <jmalek87@gmail.com>
2020-06-24 19:57:33 +0200

    type overloads

--- learn-TS/advanced-types/src/app.ts
+++ learn-TS/advanced-types/src/app.ts
@@ -126,3 +126,18 @@ const errorBag: ErrorContainer = {
 	email: "Not a valid email!",
 	username: "Must start with a capital character!",
 };
+
+// Function overloads
+function add2(a: number, b: number): number;
+function add2(a: string, b: string): string;
+function add2(a: number, b: string): string;
+function add2(a: string, b: number): string;
+function add2(a: Combinable, b: Combinable) {
+	if (typeof a === "string" || typeof b === "string") {
+		return a.toString(+b.toString());
+	}
+	return a + b;
+}
+
+const result = add2(3, 5);
+const stringResult = add2("Joanna", "Chądzyńska");
