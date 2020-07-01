d68c62940169421acff5fb1945014f8992109e30
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-01 19:08:09 +0200

    implemented autobind decorator

--- learn-TS/drag-n-drop/src/app.ts
+++ learn-TS/drag-n-drop/src/app.ts
@@ -1,3 +1,18 @@
+// decorators
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
+}
+
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
@@ -32,13 +47,14 @@ class ProjectInput {
 		this.attach();
 	}
 
+	@Autobind
 	private submitHandler(event: Event) {
 		event.preventDefault();
 		console.log(this.titleInputElement.value);
 	}
 
 	private configure() {
-		this.element.addEventListener("submit", this.submitHandler.bind(this));
+		this.element.addEventListener("submit", this.submitHandler);
 	}
 
 	private attach() {
