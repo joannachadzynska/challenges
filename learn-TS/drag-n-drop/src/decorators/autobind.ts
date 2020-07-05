b62f095f5c6bb11c616deed7756a915f7fe4123f
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-05 19:16:55 +0200

    code refractoring: using es6 modules

--- learn-TS/drag-n-drop/src/decorators/autobind.ts
+++ learn-TS/drag-n-drop/src/decorators/autobind.ts
@@ -1,16 +1,14 @@
-namespace App {
-	// autobind decorator
-	export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-		const originalMethod = descriptor.value;
+// autobind decorator
+export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
 
-		const adjDescriptor: PropertyDescriptor = {
-			configurable: true,
-			enumerable: false,
-			get() {
-				const boundFn = originalMethod.bind(this);
-				return boundFn;
-			},
-		};
-		return adjDescriptor;
-	}
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
 }
