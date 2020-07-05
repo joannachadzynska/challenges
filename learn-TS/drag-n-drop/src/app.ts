c420ca174389d68cc9c48a0a40bda45398f61732
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-05 17:17:13 +0200

    implement getter for received persons data

--- learn-TS/drag-n-drop/src/app.ts
+++ learn-TS/drag-n-drop/src/app.ts
@@ -172,6 +172,15 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 	private project: Project;
+
+	get persons() {
+		if (this.project.people === 1) {
+			return "1 person";
+		} else {
+			return `${this.project.people} persons`;
+		}
+	}
+
 	constructor(hostId: string, project: Project) {
 		super("single-project", hostId, false, project.id);
 		this.project = project;
@@ -183,9 +192,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
 	renderContent() {
 		this.element.querySelector("h2")!.textContent = this.project.title;
-		this.element.querySelector(
-			"h3"
-		)!.textContent = this.project.people.toString();
+		this.element.querySelector("h3")!.textContent = this.persons + " assigned";
 		this.element.querySelector("p")!.textContent = this.project.description;
 	}
 }
@@ -276,7 +283,7 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 		const peopleValidatable: Validatable = {
 			value: +enteredPeopleAmount,
 			required: true,
-			min: 1,
+			min: 0,
 		};
 
 		if (
