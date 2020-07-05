87a2708f9f5c2686fdbaee677e8ef6c7a68bb1f2
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-05 17:59:56 +0200

    finished drag n drop app

--- learn-TS/drag-n-drop/src/app.ts
+++ learn-TS/drag-n-drop/src/app.ts
@@ -66,7 +66,18 @@ class ProjectState extends State<Project> {
 		);
 
 		this.projects.push(newProject);
+		this.updateListeners();
+	}
 
+	moveProject(projectId: string, newStatus: ProjectStatus) {
+		const project = this.projects.find((prj) => prj.id === projectId);
+		if (project && project.status !== newStatus) {
+			project.status = newStatus;
+			this.updateListeners();
+		}
+	}
+
+	private updateListeners() {
 		for (const listenerFn of this.listeners) {
 			listenerFn(this.projects.slice());
 		}
@@ -204,7 +215,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
 
 	@Autobind
 	dragStartHandler(event: DragEvent) {
-		console.log(event);
+		event.dataTransfer!.setData("text/plain", this.project.id);
+		event.dataTransfer!.effectAllowed = "move";
 	}
 
 	dragEndHandler(_: DragEvent) {
@@ -256,12 +268,22 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
 	}
 
 	@Autobind
-	dragOverHandler(_: DragEvent) {
-		const listEl = this.element.querySelector("ul")!;
-		listEl.classList.add("droppable");
+	dragOverHandler(event: DragEvent) {
+		if (event.dataTransfer && event.dataTransfer?.types[0] === "text/plain") {
+			event.preventDefault();
+			const listEl = this.element.querySelector("ul")!;
+			listEl.classList.add("droppable");
+		}
 	}
 
-	dropHandler(_: DragEvent) {}
+	@Autobind
+	dropHandler(event: DragEvent) {
+		const prjId = event.dataTransfer!.getData("text/plain");
+		projectState.moveProject(
+			prjId,
+			this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished
+		);
+	}
 
 	@Autobind
 	dragLeaveHandler(_: DragEvent) {
