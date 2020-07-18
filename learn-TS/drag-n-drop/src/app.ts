b01e99e403e048c1c9989cc7812ba9663d000ecb
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-18 18:28:58 +0200

    install validation class packages create new test file for learning about third party libraries in TS

--- learn-TS/drag-n-drop/src/app.ts
+++ learn-TS/drag-n-drop/src/app.ts
@@ -1,8 +1,35 @@
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+
 import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
+import _ from "lodash";
+// declare var GLOBAL: any;
+import { Product } from "./product.model";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
 
-console.log("hi");
+console.log(_.shuffle([1, 2, 3, 4, 5]));
+// console.log(GLOBAL);
+
+const products = [
+	{ title: "A Carpet", price: 29.99 },
+	{ title: "A Another Book", price: 10.99 },
+];
+const p1 = new Product("", -12.99);
+validate(p1).then((errors) => {
+	if (errors.length > 0) {
+		console.log("VALIDATION ERRORS!");
+		console.log(errors);
+	}
+	console.log(p1.getInformation());
+});
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+	console.log(prod.getInformation());
+}
