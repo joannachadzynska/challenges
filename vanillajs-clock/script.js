36f86a3d40ffa2a0f65b63a52cdbbd0e578e7274
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-22 20:51:43 +0200

    implemented function for digital clock

--- vanillajs-clock/script.js
+++ vanillajs-clock/script.js
@@ -1,25 +1,59 @@
-console.log("hello clock");
+// analog clock
 const secondHand = document.querySelector(".hand__second");
 const minuteHand = document.querySelector(".hand__min");
 const hourHand = document.querySelector(".hand__hour");
-const secondsCircle = document.querySelector(".clock");
 
-console.log(secondsCircle);
+// digital clock
+const hour = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+const analogClock = document.querySelector(".clock");
+const digitalClock = document.querySelector(".digital-clock");
+
+const toggleModeBtn = document.getElementById("js-toggle-mode");
 
 function setDate() {
 	const now = new Date();
-	const seconds = now.getSeconds();
-	const secondsDegrees = (seconds / 60) * 360 + 90;
-	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	let seconds = now.getSeconds() * 6;
+	let mins = now.getMinutes() * 6;
+	let hours = now.getHours() * 30;
 
-	const mins = now.getMinutes();
-	const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
-	minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-	const hours = now.getHours();
-	const hoursDegrees = (hours / 12) * 360 + (mins / 60) * 30 + 90;
+	const hoursDegrees = hours + mins / 12;
+	secondHand.style.transform = `rotate(${seconds}deg)`;
+	minuteHand.style.transform = `rotate(${mins}deg)`;
 	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
+function setDigitalClock() {
+	let h = new Date().getHours();
+	let m = new Date().getMinutes();
+	let s = new Date().getSeconds();
+	let am = "AM";
+
+	if (h > 12) {
+		h = h - 12;
+		let am = "PM";
+	}
+
+	// add zero before single digit numbers
+	h = h < 10 ? `0${h}` : h;
+	m = m < 10 ? `0${m}` : m;
+	s = s < 10 ? `0${s}` : s;
+
+	hour.innerHTML = `${h}:`;
+	minutes.innerHTML = `${m}:`;
+	seconds.innerHTML = `${s}&nbsp;`;
+	ampm.innerHTML = `${am}`;
+}
+
 setInterval(setDate, 1000);
+setInterval(setDigitalClock, 1000);
 setDate();
+
+function toggleMode(e) {
+	console.log(e.target);
+}
+
+toggleModeBtn.addEventListener("click", toggleMode);
