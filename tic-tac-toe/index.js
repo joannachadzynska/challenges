a5eef00dceba11f53111542b8673505748c208ee
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-22 13:04:06 +0200

    finish game

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -1,1 +1,87 @@
-console.log("Welcome to tic tac toe game!");
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winningMessageElement = document.getElementById("winning-message");
+const restartButton = document.querySelector(".btn__reset");
+const winningMessageTextElement = document.querySelector(
+	"[data-winning-message-text]"
+);
+let circleTurn;
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+	cellElements.forEach((cell) => {
+		cell.classList.remove(X_CLASS);
+		cell.classList.remove(CIRCLE_CLASS);
+		cell.removeEventListener("click", handleClick);
+		cell.addEventListener("click", handleClick, { once: true });
+	});
+	setBoardHoverClass();
+	winningMessageElement.classList.remove("show");
+}
+
+function handleClick(e) {
+	const cell = e.target;
+	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+	placeMark(cell, currentClass);
+
+	checkWin(currentClass)
+		? endGame(false)
+		: isDraw()
+		? endGame(true)
+		: swapTurns() || setBoardHoverClass();
+}
+
+function placeMark(cell, currentClass) {
+	cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+	circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+	board.classList.remove(X_CLASS);
+	board.classList.remove(CIRCLE_CLASS);
+	circleTurn ? board.classList.add(CIRCLE_CLASS) : board.classList.add(X_CLASS);
+}
+
+function checkWin(currentClass) {
+	return WINNING_COMBINATIONS.some((combination) => {
+		return combination.every((index) => {
+			return cellElements[index].classList.contains(currentClass);
+		});
+	});
+}
+
+function endGame(draw) {
+	draw
+		? (winningMessageTextElement.innerText = "Draw!")
+		: (winningMessageTextElement.innerText = `${
+				circleTurn ? "O's" : "X's"
+		  } Wins!`);
+
+	winningMessageElement.classList.add("show");
+}
+
+function isDraw() {
+	return [...cellElements].every(
+		(cell) =>
+			cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+	);
+}
