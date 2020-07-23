d75084b861a6bad4c2c2dec6c0c8c2245f53d864
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-23 20:02:59 +0200

    setup redux-devtools-extension

--- swapi-api-ver2/src/stores/store.ts
+++ swapi-api-ver2/src/stores/store.ts
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { rootReducer } from '../reducers/rootReducer';
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { rootReducer } from "../reducers/rootReducer";
 
 export const store = createStore(
 	rootReducer,
-	applyMiddleware(thunkMiddleware)
+	composeWithDevTools(applyMiddleware(thunkMiddleware))
 );
