a28ae42cc81ae37fdeabdcc5d951be579f76c003
Joanna Chądzyńska <jmalek87@gmail.com>
2020-07-23 19:50:21 +0200

    setup new swapi app

---
+++ swapi-api-ver2/src/components/About/About.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+export const About: React.FC = () => (
+	<article>
+		This boilerplate is made by Mateusz "doman89" Domański.
+	</article>
+);
---
+++ swapi-api-ver2/webpack/common.js
@@ -0,0 +1,64 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path').resolve;
+const WebpackHtmlPlugin = require('html-webpack-plugin');
+
+const destinationPath = path(__dirname, '..', 'build');
+const sourcePath = path(__dirname, '..', 'src');
+
+module.exports = {
+	entry: {
+		index: path(sourcePath, 'index.ts'),
+	},
+	output: {
+		path: path(destinationPath),
+		chunkFilename: 'js/[name].[contenthash:6].js',
+		filename: '[name].[contenthash:6].js',
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				include: sourcePath,
+				options: {
+					transpileOnly: true,
+				},
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(eot|ttf|woff|woff2)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash:8].[ext]',
+						outputPath: 'static/fonts',
+						publicPath: 'static/fonts',
+					},
+				},
+			},
+			{
+				test: /\.(a?png|svg|jpe?g|gif|bmp)$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 8192,
+						name: '[name].[hash:8].[ext]',
+						outputPath: 'static/images',
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new ForkTsCheckerWebpackPlugin({
+			useTypescriptIncrementalApi: true,
+		}),
+
+		new WebpackHtmlPlugin ({
+			template: path(__dirname, '..', 'public', 'index.html'),
+		}),
+	],
+};
