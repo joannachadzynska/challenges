a263a99a9936c7498c03e89f2677466846330a2d
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-11 21:19:05 +0200

    update global styles

---
+++ spacex-graphql/src/styles/globalStyles.ts
@@ -0,0 +1,40 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import { boxStyles, linkStyles } from '../themes/myTheme';
+
+export const Div = styled.div`
+	max-width: 100xp;
+`;
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        ${boxStyles}
+        margin: 0;
+	    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+		'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+		sans-serif;
+	    -webkit-font-smoothing: antialiased;
+	    -moz-osx-font-smoothing: grayscale;
+	    min-height: 100vh;
+	    max-width: 960px;
+        margin: 0 auto;
+    }
+
+
+	a {
+		${linkStyles};
+		text-decoration: none;
+	}
+
+    ul {
+	list-style-type: none;
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+}
+`;
