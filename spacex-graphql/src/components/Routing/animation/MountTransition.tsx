ad5b82468a60a5401ad9595f12e34cad2dbb3373
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-12 21:42:41 +0200

    add transition

--- spacex-graphql/src/components/Routing/animation/MountTransition.tsx
+++ spacex-graphql/src/components/Routing/animation/MountTransition.tsx
@@ -14,7 +14,8 @@ export const MountTransition: FC<Props> = ({
 	<motion.div
 		exit={{ opacity: 0, x: slide, y: slideUp }}
 		initial={{ opacity: 0, x: slide, y: slideUp }}
-		animate={{ opacity: 1, x: 0, y: 0 }}>
+		animate={{ opacity: 1, x: 0, y: 0 }}
+		transition={{ ease: 'easeInOut', delay: 0.5, duration: 0.5 }}>
 		{children}
 	</motion.div>
 );
