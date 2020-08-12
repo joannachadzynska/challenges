221870140f7f94eab0bbdbe8279b53dc6cffa842
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-12 20:49:54 +0200

    Add wrapper to content

--- spacex-graphql/src/components/Layout/Content/Content.tsx
+++ spacex-graphql/src/components/Layout/Content/Content.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Routes from '../../Routing';
+import { Wrapper } from '../../../styles/Wrapper';
 
 export interface ContentProps {}
 
-const Content: React.SFC<ContentProps> = () => {
-	return (
-		<div>
-			<Routes />
-		</div>
-	);
-};
+const Content: React.SFC<ContentProps> = () => (
+	<Wrapper>
+		<Routes />
+	</Wrapper>
+);
 
 export default Content;
