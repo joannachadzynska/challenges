964a131796da38df9d091b3f153687204f1136d5
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-12 21:42:53 +0200

    add new animation

--- spacex-graphql/src/components/pages/RocketDetails/RocketDetails.tsx
+++ spacex-graphql/src/components/pages/RocketDetails/RocketDetails.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-apollo-hooks';
+import { motion, AnimatePresence } from 'framer-motion';
 import { GET_ROCKET_DETAILS } from '../../../queries/getRocketDetails';
 import { Rocket } from '../../../models/Rocket';
 import { LoadingIndicator } from '../../shared';
@@ -11,6 +12,7 @@ interface GraphQlResult {
 }
 
 const RocketDetails: React.SFC = () => {
+	const [showTitle, setShowTitle] = React.useState(true);
 	const { id } = useParams();
 
 	const { loading, error, data } = useQuery<GraphQlResult>(GET_ROCKET_DETAILS, {
@@ -23,8 +25,18 @@ const RocketDetails: React.SFC = () => {
 	if (error) return <p>Oops! Something went wrong!</p>;
 	if (!data?.rocket) return <p>There is no data to display</p>;
 
+	setTimeout(() => {
+		setShowTitle(false);
+	}, 2000);
+
 	return (
 		<div>
+			<AnimatePresence>
+				{showTitle && (
+					<motion.h2 exit={{ x: -1000 }}>Rocket Details:</motion.h2>
+				)}
+			</AnimatePresence>
+
 			<RocketName {...data.rocket} />
 			<RocketCountry {...data.rocket} />
 			<RocketDescription {...data.rocket} />
