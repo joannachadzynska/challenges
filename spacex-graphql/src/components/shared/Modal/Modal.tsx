b50f097cb8707f82cec44439f78679f7a3e7a4ce
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-14 11:38:43 +0200

    add modal animation

--- spacex-graphql/src/components/shared/Modal/Modal.tsx
+++ spacex-graphql/src/components/shared/Modal/Modal.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
+import { AnimatePresence } from 'framer-motion';
 import { ModalContainer, ModalOverlay } from './styles';
 import { Button } from '../Button/styles';
+
 const modalsContainer = document.getElementById('portal')!;
 
 export interface ModalProps {
@@ -9,24 +11,52 @@ export interface ModalProps {
 	onClose: () => void;
 }
 
+const overlay = {
+	visible: {
+		opacity: 1,
+	},
+	hidden: {
+		opacity: 0,
+	},
+};
+
+const modal = {
+	visible: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			delay: 0.5,
+		},
+	},
+	hidden: {
+		opacity: 0,
+		y: '-100vh',
+	},
+};
+
 const Modal: React.SFC<ModalProps> = ({ children, isOpen, onClose }) =>
 	isOpen &&
 	createPortal(
-		<>
-			<ModalOverlay />
-			<ModalContainer>
-				<h2>MODAL TITLE</h2>
-				<p>
-					Check where I am mounted in the DOM{' '}
-					<span role='img' aria-label='emoji1'>
-						😃
-					</span>
-				</p>
-				<Button variant='primary' onClick={onClose}>
-					CloseMe
-				</Button>
-			</ModalContainer>
-		</>,
+		<AnimatePresence exitBeforeEnter>
+			<ModalOverlay
+				variants={overlay}
+				animate='visible'
+				initial='hidden'
+				exit='hidden'>
+				<ModalContainer variants={modal} animate='visible' initial='hidden'>
+					<h2>MODAL TITLE</h2>
+					<p>
+						Check where I am mounted in the DOM{' '}
+						<span role='img' aria-label='emoji1'>
+							😃
+						</span>
+					</p>
+					<Button variant='primary' onClick={onClose}>
+						CloseMe
+					</Button>
+				</ModalContainer>
+			</ModalOverlay>
+		</AnimatePresence>,
 		modalsContainer
 	);
 
--- spacex-graphql/src/components/shared/Modal/styles.ts
+++ spacex-graphql/src/components/shared/Modal/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const ModalContainer = styled.div`
+export const ModalContainer = styled(motion.div)`
 	background: azure;
 	border: 5px solid black;
 	height: 35vh;
@@ -10,15 +11,14 @@ export const ModalContainer = styled.div`
 	overflow: auto;
 	padding: 20px;
 	text-align: center;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
 	z-index: 1010;
 `;
 
-export const ModalOverlay = styled.div`
+export const ModalOverlay = styled(motion.div)`
 	background-color: rgba(0, 0, 0, 0.6);
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	position: fixed;
 	top: 0;
 	left: 0;
