ba2da8d5a5f3689094b3a0e745941c4f57026601
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-14 14:19:31 +0200

    create theme switch slider

---
+++ spacex-graphql/src/components/Layout/Header/ThemeSwitch.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+	ThemeSwitchWrapper,
+	ThemeSwitchLabel,
+	ThemeSwitchSlider,
+} from './styles';
+
+export interface ThemeSwitchProps {
+	switchTheme: () => void;
+}
+
+const ThemeSwitch: React.SFC<ThemeSwitchProps> = ({ switchTheme }) => {
+	return (
+		<ThemeSwitchWrapper>
+			<ThemeSwitchLabel htmlFor='checkbox'>
+				<input type='checkbox' id='checkbox' onChange={switchTheme} />
+				<ThemeSwitchSlider />
+			</ThemeSwitchLabel>
+			<em>Enable Dark Mode!</em>
+		</ThemeSwitchWrapper>
+	);
+};
+
+export default ThemeSwitch;
