5bc8d1292225c70c2afd83a9e8c86a0e5524d8dc
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-25 20:07:58 +0200

    remove section tag

--- spacex-graphql/src/components/Routing/Routes.tsx
+++ spacex-graphql/src/components/Routing/Routes.tsx
@@ -13,31 +13,29 @@ import { RouteTransition, AnimatedRoutes } from './animation/RouteTransition';
 
 const Routes: React.SFC = () => {
 	return (
-		<section>
-			<AnimatedRoutes exitBeforeEnter initial={false}>
-				<RouteTransition exact path='/' slideUp={30}>
-					<Landing />
-				</RouteTransition>
-				<RouteTransition path='/about' slideUp={30}>
-					<About />
-				</RouteTransition>
-				<RouteTransition path='/launches' slideUp={30}>
-					<LaunchesPastListing />
-				</RouteTransition>
-				<RouteTransition path='/missions' slideUp={30}>
-					<MissionsListing />
-				</RouteTransition>
-				<RouteTransition path='/rockets' slideUp={30}>
-					<RocketListing />
-				</RouteTransition>
-				<RouteTransition path='/rocket/:id' slideUp={30}>
-					<RocketDetails />
-				</RouteTransition>
-				<RouteTransition path='*' slideUp={30}>
-					<NotFound />
-				</RouteTransition>
-			</AnimatedRoutes>
-		</section>
+		<AnimatedRoutes exitBeforeEnter initial={false}>
+			<RouteTransition exact path='/' slideUp={30}>
+				<Landing />
+			</RouteTransition>
+			<RouteTransition path='/about' slideUp={30}>
+				<About />
+			</RouteTransition>
+			<RouteTransition path='/launches' slideUp={30}>
+				<LaunchesPastListing />
+			</RouteTransition>
+			<RouteTransition path='/missions' slideUp={30}>
+				<MissionsListing />
+			</RouteTransition>
+			<RouteTransition path='/rockets' slideUp={30}>
+				<RocketListing />
+			</RouteTransition>
+			<RouteTransition path='/rocket/:id' slideUp={30}>
+				<RocketDetails />
+			</RouteTransition>
+			<RouteTransition path='*' slideUp={30}>
+				<NotFound />
+			</RouteTransition>
+		</AnimatedRoutes>
 	);
 };
 
