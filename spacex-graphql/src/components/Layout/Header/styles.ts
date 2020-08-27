ffa9c4101402ff4ef62dbb5bfc7e4e82aa7e2cff
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-27 22:57:42 +0200

    update styles

--- spacex-graphql/src/components/Layout/Header/styles.ts
+++ spacex-graphql/src/components/Layout/Header/styles.ts
@@ -4,6 +4,10 @@ import { motion } from 'framer-motion';
 import { textColor } from '../../../themes/myTheme';
 import { mediaQueries } from '../../../themes/mediaTemplates';
 
+type Props = {
+	open: boolean;
+};
+
 export const StyledHeader = styled.header`
 	grid-area: header;
 `;
@@ -15,35 +19,48 @@ export const Navbar = styled(motion.nav)`
 export const NavbarContainer = styled.div`
 	border-bottom: 5px solid #fff;
 	display: flex;
-	justify-content: flex-end;
+	justify-content: flex-start;
 	align-items: center;
 	padding-bottom: 0.1em;
+
 	${mediaQueries('md')`
     justify-content: space-between
   `};
 `;
 
-export const NavbarLinks = styled.nav`
-	ul {
-		display: flex;
-		align-items: center;
-		justify-content: space-evenly;
-		margin: 0;
-		padding: 0;
+export const NavbarLinks = styled(motion.ul)`
+	margin: 0;
+	padding: 0;
+	padding: 25px;
+	position: absolute;
+	top: 100px;
+	right: 0;
+	width: 230px;
 
-		li:last-of-type > a {
-			padding-right: 0;
-		}
-	}
+	${mediaQueries('md')`
+    position: static;
+    display: flex;
+    align-items: center;
+	justify-content: space-between;
+    width: auto;
+    padding: 0;
+    text-align: right;
+  `};
 `;
 
-export const NavbarLink = styled(NavLink)`
-	/* color: ${textColor}; */
-	color: #fff;
+export const NavbarLink = styled(NavLink)<Props>`
+	color: #000;
+	cursor: pointer;
 	font-weight: 500;
 	font-size: 1.5rem;
-	display: block;
+	display: ${(props) => (props.open ? 'block' : 'none')};
 	padding: 1rem;
+	transition-delay: 0.3s;
+
+	${mediaQueries('md')`
+    display: block;
+    color: #fff;
+  `};
 `;
 
 export const NavbarBrand = styled(motion.div)`
@@ -67,66 +84,4 @@ export const RocketSvg = styled(motion.svg)`
 export const LogoLink = styled(NavbarLink)`
 	font-size: 3rem;
 	padding: 0;
-`;
-
-// Theme Switch component
-export const ThemeSwitchWrapper = styled.div`
-	display: flex;
-	align-items: center;
-
-	em {
-		margin-left: 10px;
-		font-size: 1rem;
-	}
-`;
-
-export const ThemeSwitchSlider = styled(motion.div)`
-	background: linear-gradient(#39598a, #79d7ed);
-	bottom: 0;
-	border: 1px solid #fff;
-	border-radius: 34px;
-	cursor: pointer;
-	height: 100%;
-	position: relative;
-	transition: 0.4s;
-
-	&::before {
-		color: yellow;
-		content: '\f185';
-		font-family: FontAwesome;
-		font-size: 22px;
-		position: absolute;
-		left: 4px;
-		transition: 0.4s;
-	}
-`;
-
-export const ThemeSwitchLabel = styled.label`
-	display: inline-block;
-	height: 34px;
-	position: relative;
-	width: 60px;
-
-	input {
-		display: none;
-	}
-
-	input + ${ThemeSwitchSlider}::before {
-		transform: ${(props: any) =>
-			props.theme === 'light' ? 'translateX(0)' : 'translateX(26px)'};
-		/* content: ${(props: any) =>
-			props.theme === 'dark' ? '\f186' : `\f185`}; */
-	}
-
-	input:checked + ${ThemeSwitchSlider} {
-		background: linear-gradient(#091236, #1e215d);
-	}
-
-	input:checked + ${ThemeSwitchSlider}::before {
-		transform: ${(props: any) =>
-			props.theme === 'light' ? 'translateX(26px)' : 'translateX(0)'};
-		color: azure;
-		content: '\f186';
-		font-family: FontAwesome;
-	}
 `;
