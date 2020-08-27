d3b352747d7ac82756778058b784793df5301d3c
Joanna Chądzyńska <jmalek87@gmail.com>
2020-08-27 22:49:04 +0200

    remove unused components

--- spacex-graphql/src/components/Layout/Header/Menu/MotionLink.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-
-const MotionLink: React.SFC = ({ children }) => {
-	return <motion.li whileHover={{ filter: `invert()` }}>{children}</motion.li>;
-};
-
-export default MotionLink;
--- spacex-graphql/src/components/Layout/Header/MenuMobile/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MenuToggle } from './MenuToggle';
-export { default as NavigationMobile } from './NavigationMobile';
-export { default as MenuMobile } from './MenuMobile';
