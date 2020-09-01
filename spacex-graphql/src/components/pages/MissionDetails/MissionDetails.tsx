3fbe0786dc42a4e5d46c9286c4df44195f4799f3
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-01 15:22:11 +0200

    create missionn details page

---
+++ spacex-graphql/src/components/pages/MissionDetails/MissionDetails.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export interface MissionDetailsProps {}
+
+const MissionDetails: React.SFC<MissionDetailsProps> = () => {
+	return (
+		<div>
+			<h2>mission details</h2>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
+				doloribus, fugiat ducimus omnis temporibus quasi doloremque porro
+				accusantium culpa libero at eveniet quod nesciunt quo est nihil magnam
+				tempore necessitatibus nisi, molestias, magni amet! In porro possimus ab
+				expedita labore, temporibus odio magnam debitis hic fugit voluptatem
+				accusantium accusamus, rem minus aperiam inventore omnis mollitia harum.
+				Temporibus tempore hic recusandae quas est itaque qui ad a accusantium
+				nesciunt. Repellat quaerat facere harum repudiandae ad recusandae
+				accusamus, maiores incidunt commodi, soluta nobis numquam, tempora quas
+				odio nulla molestiae eos officiis. Provident hic, asperiores cum
+				praesentium officiis non quasi quia necessitatibus fuga?
+			</p>
+		</div>
+	);
+};
+
+export default MissionDetails;
