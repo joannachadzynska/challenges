c961ae3b6a0e0986c2154d10e2cf050ac868bc37
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-02 20:31:45 +0200

    setup theme

--- spacex-graphql/src/@types/styled.d.ts
+++ spacex-graphql/src/@types/styled.d.ts
@@ -1,13 +1,31 @@
 import 'styled-components';
 
-// declare module 'styled-components' {
-// 	export interface DefaultTheme {
-// 		borderRadius: string;
-// 		mode: string;
-
-// 		colors: {
-// 			main: string;
-// 			secondary: string;
-// 		};
-// 	}
-// }
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		colors: {
+			green: {
+				light: string;
+				dark: string;
+			};
+			yellow: {
+				light: string;
+			};
+			blue: {
+				dark: string;
+				primary: string;
+				veryDark: string;
+			};
+			white: string;
+			red: string;
+		};
+		fonts: {
+			mavenPro: string;
+			montserrat: string;
+		};
+		fontSizes: {
+			small: string;
+			medium: string;
+			large: string;
+		};
+	}
+}
---
+++ spacex-graphql/src/themes/theme.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+	colors: {
+		green: {
+			light: '#25D225',
+			dark: '#008000',
+		},
+		yellow: {
+			light: '#F6C744',
+		},
+		blue: {
+			dark: '#00395C',
+			primary: '#0094FF',
+			veryDark: '#002235',
+		},
+		white: '#ffffff',
+		red: '#ff0000',
+	},
+	fonts: {
+		mavenPro: "'Maven Pro', sans-serif",
+		montserrat: "'Montserrat', sans-serif",
+	},
+	fontSizes: {
+		small: '1rem',
+		medium: '2rem',
+		large: '3rem',
+	},
+};
+
+const Theme: React.SFC = ({ children }) => (
+	<ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default Theme;
