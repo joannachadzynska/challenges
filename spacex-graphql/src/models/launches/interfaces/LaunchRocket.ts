e64b48333098394691fcfb217a20879473d3e43e
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-03 21:55:25 +0200

    create new interfaces

--- spacex-graphql/src/models/launches/interfaces/LaunchRocket.ts
+++ spacex-graphql/src/models/launches/interfaces/LaunchRocket.ts
@@ -1,5 +1,27 @@
 import { Rocket } from '../../rockets/interfaces/Rocket';
+import { Payload } from '../../payloads/interfaces/Payload';
 
 export interface LaunchRocket {
+	first_stage: LaunchRocketFirstStage;
+	rocket_name: string;
+	rocket_type: string;
 	rocket: Rocket;
+	second_stage: LaunchRocketSecondStage;
+}
+
+export interface LaunchRocketFirstStage {
+	cores: LaunchRocketFirstStageCore[];
+}
+
+export interface LaunchRocketFirstStageCore {
+	flight: number;
+	land_success: boolean;
+	landing_intent: boolean;
+	landing_type?: string;
+	landing_vehicle: string;
+	reused: boolean;
+}
+
+export interface LaunchRocketSecondStage {
+	payloads: Payload[];
 }
