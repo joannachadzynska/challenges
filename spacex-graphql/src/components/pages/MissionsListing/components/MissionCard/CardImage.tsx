7c78120ffccc43868d530c091b5193f1a5ef6834
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-03 19:39:08 +0200

    update card image: add multiple sources

--- spacex-graphql/src/components/pages/MissionsListing/components/MissionCard/CardImage.tsx
+++ spacex-graphql/src/components/pages/MissionsListing/components/MissionCard/CardImage.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { LaunchViewModel } from '../../../../../models/launches/viewModels/LaunchViewModel';
 import { CardImageWrapper, CardImg } from './styles';
+import spaceX from '../../../../../assets/images/spacex.jpg';
 
 type CardImageProps = Pick<LaunchViewModel, 'links' | 'launchSuccess'>;
 
-const CardImage: React.SFC<CardImageProps> = ({ links, launchSuccess }) => (
-	<CardImageWrapper launchSuccess={launchSuccess}>
-		<CardImg src={links.mission_patch_small} alt='Mission patch small' />
-	</CardImageWrapper>
-);
+const CardImage: React.SFC<CardImageProps> = ({ links, launchSuccess }) => {
+	return (
+		<CardImageWrapper launchSuccess={launchSuccess}>
+			<CardImg
+				src={
+					links.flickr_images.length > 0 ||
+					links.mission_patch ||
+					links.mission_patch_small
+						? links.mission_patch_small
+							? links.mission_patch_small
+							: links.flickr_images[1]
+						: spaceX
+				}
+				alt='Mission patch small'
+			/>
+		</CardImageWrapper>
+	);
+};
 
 export default CardImage;
