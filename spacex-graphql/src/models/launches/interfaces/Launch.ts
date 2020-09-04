6b4defd3981f3c4a86175a2ffc2f6278b7bcf67f
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-04 20:32:25 +0200

    update launch interface

--- spacex-graphql/src/models/launches/interfaces/Launch.ts
+++ spacex-graphql/src/models/launches/interfaces/Launch.ts
@@ -29,6 +29,11 @@ export interface LaunchLinks {
 	flickr_images: string[];
 	mission_patch_small?: string;
 	mission_patch?: string;
+	presskit: string;
+	reddit_campaign?: string;
+	reddit_launch?: string;
+	reddit_media?: string;
+	reddit_recovery?: string;
 	video_link: string;
 	wikipedia: string;
 }
