4b113a84e26e83df4aa914d07e8bcaddc407fcb7
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-04 18:13:18 +0200

    update styles

---
+++ spacex-graphql/src/components/pages/MissionDetails/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import { mediaQueries } from '../../../themes/mediaTemplates';
+
+export const SectionTitle = styled.h2`
+	font-size: 1.5rem;
+	font-weight: 400;
+	letter-spacing: 1px;
+	line-height: 1.1;
+	margin: 1em 0;
+	text-transform: uppercase;
+
+	${mediaQueries('md')`
+    	font-size: 2rem;
+  `};
+`;
