c3080f3cc7998987024d65448de501419a361ec6
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-21 15:13:32 +0200

    remove comments

--- spacex-graphql/src/App.tsx
+++ spacex-graphql/src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Header, Footer, Content } from './components/Layout';
-// import { useDarkMode } from './hooks/useDarkMode';
 import { GlobalStyle } from './styles/globalStyles';
 import Theme from './themes/theme';
 
 const App: React.SFC = () => {
-	// const { theme, themeToggler, mountedComponent } = useDarkMode();
-
-	// if (!mountedComponent) return <div />;
-
 	return (
 		<Theme>
 			<GlobalStyle />
