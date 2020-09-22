3b63b865d3a4bf056f8ea735f809d6d60b102209
Joanna Chądzyńska <jmalek87@gmail.com>
2020-09-22 14:48:48 +0200

    add util file with methods

---
+++ spacex-graphql/src/utils/date.ts
@@ -0,0 +1,17 @@
+export const getNumberWithOrdinal = (n: number) => {
+	var s = ['th', 'st', 'nd', 'rd'],
+		v = n % 100;
+	return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
+
+export const setDateToString = (date: Date) => {
+	let options = { month: 'long', day: 'numeric', year: 'numeric' };
+	const splitDate = date.toLocaleDateString('en-US', options).split(' ');
+	const month = splitDate[0];
+	const day = parseInt(splitDate[1]);
+
+	const dayWithOrdinal = getNumberWithOrdinal(day);
+	const year = splitDate[2];
+
+	return `${month} ${dayWithOrdinal} ${year}`;
+};
