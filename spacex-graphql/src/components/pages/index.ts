be1ef25d3e4864b765506910c7c173799de6ef9f
Joanna Chądzyńska <jmalek87@gmail.com>
2020-10-22 20:42:43 +0200

    remove landing page

--- spacex-graphql/src/components/pages/index.ts
+++ spacex-graphql/src/components/pages/index.ts
@@ -1,7 +1,6 @@
 export { default as About } from './About';
-export { default as Landing } from './Landing';
-export { default as MissionsListing } from './MissionsListing';
 export { default as MissionDetails } from './MissionDetails';
+export { default as MissionsListing } from './MissionsListing';
 export { default as NotFound } from './NotFound';
 export { default as RocketDetails } from './RocketDetails';
 export { default as RocketListing } from './RocketsListing';
