bf4fe4e6bcecd5af867a25c19a6ad94664d72499
Joanna Chądzyńska <jmalek87@gmail.com>
2020-10-22 15:22:40 +0200

    add eslint line

--- spacex-graphql/src/hooks/useCountdown.tsx
+++ spacex-graphql/src/hooks/useCountdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { setInterval } from 'timers';
 import { calculateTimeLeft } from 'utils/date';
