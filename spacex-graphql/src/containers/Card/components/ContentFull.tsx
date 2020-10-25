bdfde733e857c7d21cca842e36bcc1c715eca050
Joanna Chądzyńska <jmalek87@gmail.com>
2020-10-25 15:39:51 +0100

    create new component

---
+++ spacex-graphql/src/containers/Card/components/ContentFull.tsx
@@ -0,0 +1,33 @@
+import { Card } from 'components';
+import { LaunchViewModel } from 'models/launches/viewModels/LaunchViewModel';
+import React from 'react';
+import { setDateToString } from 'utils/date';
+
+type ContentFullProps = Pick<
+	LaunchViewModel,
+	'id' | 'launchDateLocal' | 'details' | 'missionName'
+>;
+
+const ContentFull: React.SFC<ContentFullProps> = ({
+	details,
+	launchDateLocal,
+	id,
+	missionName,
+}) => {
+	return (
+		<Card.Content direction='cols'>
+			<Card.Title to={`/mission/${id}`}>{missionName}</Card.Title>
+			<Card.Date>
+				<time dateTime={launchDateLocal.toISOString()}>
+					{setDateToString(launchDateLocal)}
+				</time>
+			</Card.Date>
+			<Card.Break />
+			<Card.FullDescription>
+				<p>{details}</p>
+			</Card.FullDescription>
+		</Card.Content>
+	);
+};
+
+export default ContentFull;
