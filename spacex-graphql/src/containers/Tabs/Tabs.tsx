5fac513c332653e57ff090983fff66eb5055ef34
Joanna Chądzyńska <jmalek87@gmail.com>
2020-10-25 15:38:45 +0100

    fix route

--- spacex-graphql/src/containers/Tabs/Tabs.tsx
+++ spacex-graphql/src/containers/Tabs/Tabs.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Tabs: React.SFC = () => {
 	const navLinks = [
 		{ id: 1, name: 'All Missions', path: `${ROUTES.MISSIONS}` },
-		{ id: 2, name: 'Latest Mission', path: `${ROUTES.MISSIONS_UPCOMING}` },
+		{ id: 2, name: 'Latest Mission', path: `${ROUTES.MISSIONS_LATEST}` },
 		{ id: 3, name: 'Next Mission', path: `${ROUTES.MISSIONS_NEXT}` },
 		{ id: 4, name: 'Past Missions', path: `${ROUTES.MISSIONS_PAST}` },
 		{ id: 5, name: 'Upcoming Missions', path: `${ROUTES.MISSIONS_UPCOMING}` },
