151be9f069adb44ba3abfca46b0b4df13b141a90
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-02 14:37:44 +0100

    add query to mission details page

--- spacex-graphql/src/pages/MissionDetails.tsx
+++ spacex-graphql/src/pages/MissionDetails.tsx
@@ -1,13 +1,36 @@
 import { Footer, Header } from 'containers';
+import { Launch } from 'models/launches/interfaces/Launch';
+import { GET_LAUNCH_DETAILS } from 'queries/launchQueries/getLaunch';
 import React from 'react';
+import { useQuery } from 'react-apollo-hooks';
+import { useParams } from 'react-router-dom';
 
-export interface MissionDetailsProps {}
+type GraphQlResponse = {
+	launch: Launch;
+};
 
-const MissionDetails: React.SFC<MissionDetailsProps> = () => {
+const MissionDetails: React.SFC = () => {
+	const { id } = useParams<{ id: string }>();
+
+	const { loading, error, data } = useQuery<GraphQlResponse>(
+		GET_LAUNCH_DETAILS,
+		{
+			variables: {
+				id: id,
+			},
+		}
+	);
+
+	if (loading) return <div>loading ...</div>;
+	if (error) return <p>Oops! Something went wrong!</p>;
+	if (!data?.launch) return <p>There is no data to display</p>;
+
+	console.log(data.launch);
+
 	return (
 		<>
 			<Header />
-			<div>mission details</div>
+			<section>mission details</section>
 			<Footer />
 		</>
 	);
