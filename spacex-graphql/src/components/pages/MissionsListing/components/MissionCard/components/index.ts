40045c89fbae382a53f4ddd8b210aa59cd00b750
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-02 14:37:20 +0100

    remove files and folders

--- spacex-graphql/src/components/pages/MissionsListing/components/MissionCard/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CardContent } from './CardContent';
-export { default as CardImage } from './CardImage';
-export { default as DetailsButton } from './DetailsButton';
-export { default as MissionSuccess } from './MissionSuccess';
