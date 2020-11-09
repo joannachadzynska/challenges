b919024a1639bb188affd5437eee2169811fadde
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-09 20:02:01 +0100

    remove files

--- spacex-graphql/src/components/pages/MissionDetails/components/DetailsCard/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DetailsContent } from './DetailsContent';
-export { default as DetailsImage } from './DetailsImage';
-export { default as DetailsLaunchDescription } from './DetailsLaunchDescription';
-export { default as LaunchInfo } from './LaunchInfo';
--- spacex-graphql/src/components/pages/MissionDetails/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MissionDetailsCard } from './MissionDetailsCard';
