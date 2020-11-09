717f00ce6b7dba2ac9b18b4373dd6daf1d6d2222
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-09 19:22:33 +0100

    add location add button link to mission details

--- spacex-graphql/src/containers/Card/components/ContentFull.tsx
+++ spacex-graphql/src/containers/Card/components/ContentFull.tsx
@@ -1,6 +1,7 @@
 import { Card } from 'components';
 import { LaunchViewModel } from 'models/launches/viewModels/LaunchViewModel';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { setDateToString } from 'utils/date';
 
 type ContentFullProps = Pick<
@@ -14,6 +15,8 @@ const ContentFull: React.SFC<ContentFullProps> = ({
 	id,
 	missionName,
 }) => {
+	const location = useLocation();
+
 	return (
 		<Card.Content direction='cols'>
 			<Card.Title to={`/mission/${id}`}>{missionName}</Card.Title>
@@ -24,6 +27,13 @@ const ContentFull: React.SFC<ContentFullProps> = ({
 			</Card.Date>
 			<Card.Break />
 			<Card.FullDescription>{details}</Card.FullDescription>
+
+			{location.pathname.includes('missions') && (
+				<>
+					<Card.Break />
+					<Card.Button to={`/mission/${id}`}>More Details</Card.Button>
+				</>
+			)}
 		</Card.Content>
 	);
 };
