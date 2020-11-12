8efc53841f4049e02234a96b1e8cff58ebe21717
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-12 19:35:22 +0100

    add vehicles listing

--- spacex-graphql/src/containers/Vehicles/VehiclesContainer.tsx
+++ spacex-graphql/src/containers/Vehicles/VehiclesContainer.tsx
@@ -1,13 +1,12 @@
-import { Video } from 'components';
+import { Vehicle, Video } from 'components';
 import { LoadingIndicator } from 'components/shared';
 import { Rockets } from 'models/rockets/interfaces/Rocket';
 import { GET_ROCKETS } from 'queries/rocketQueries/getRockets';
 import React from 'react';
 import { useQuery } from 'react-apollo-hooks';
+import { SectionTitle } from 'styles/SectionTitle';
 
-export interface VehiclesContainerProps {}
-
-const VehiclesContainer: React.SFC<VehiclesContainerProps> = () => {
+const VehiclesContainer: React.SFC = () => {
 	const { loading, error, data } = useQuery<Rockets>(GET_ROCKETS);
 	if (loading) return <LoadingIndicator />;
 	if (error) return <p>Error...</p>;
@@ -22,6 +21,29 @@ const VehiclesContainer: React.SFC<VehiclesContainerProps> = () => {
 					<Video.Title>ROCKETS</Video.Title>
 				</Video.Placeholder>
 			</Video>
+			<SectionTitle>Rockets</SectionTitle>
+			<Vehicle.Description>
+				The rockets below are the past, current and yet to be active vehicles in
+				the SpaceX lineup. The proportion of the rockets are approximate but
+				somewhat accurate in their relative scale. As vehicles become available,
+				they will be added here for reference.
+			</Vehicle.Description>
+
+			<Vehicle.Group>
+				{data.rockets.map((rocket) => (
+					<Vehicle.Lineup key={rocket.id}>
+						<Vehicle.Name>{rocket.name}</Vehicle.Name>
+
+						<Vehicle>
+							<Vehicle.Image src={rocket.id} />
+						</Vehicle>
+
+						<Vehicle.Link to={`/vehicle/${rocket.id}`}>
+							Rocket Stats
+						</Vehicle.Link>
+					</Vehicle.Lineup>
+				))}
+			</Vehicle.Group>
 		</section>
 	);
 };
