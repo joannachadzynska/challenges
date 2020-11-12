b8c27694254fc8b9922d2444448c134bac9eac23
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-12 19:35:07 +0100

    create and update vehicle component

--- spacex-graphql/src/components/Vehicle/index.tsx
+++ spacex-graphql/src/components/Vehicle/index.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react';
 import {
+	ButtonLink,
 	Container,
+	Description,
+	Group,
 	Image,
 	ImageWrapper,
 	Inner,
+	Lineup,
+	Name,
 	VehicleImageWrapper,
 } from './styles/vehicle';
 
 export interface VehicleComposition {
+	Description: React.FC;
 	Image: React.FC<ImageProps>;
+	Name: React.FC;
+	Group: React.FC;
+	Link: React.FC<LinkProps>;
+	Lineup: React.FC;
 }
 
 export interface ImageProps {
@@ -16,17 +26,15 @@ export interface ImageProps {
 	alt?: string;
 }
 
+export interface LinkProps {
+	to?: any;
+}
+
 const Vehicle: React.SFC & VehicleComposition = ({
 	children,
 	...restProps
 }) => {
-	return (
-		<Container {...restProps}>
-			<Inner>
-				<ImageWrapper>{children}</ImageWrapper>
-			</Inner>
-		</Container>
-	);
+	return <Container {...restProps}>{children}</Container>;
 };
 
 export const VehicleImage: React.FC<ImageProps> = ({
@@ -34,16 +42,71 @@ export const VehicleImage: React.FC<ImageProps> = ({
 	alt,
 	...restProps
 }) => {
+	const setHeight = () => {
+		let height = '';
+		if (src === 'falcon1') {
+			height = '200';
+		}
+		if (src === 'falcon9') {
+			height = '400';
+		}
+		if (src === 'falconheavy') {
+			height = '425';
+		}
+		if (src === 'starship') {
+			height = '850';
+		}
+
+		return height;
+	};
+
 	return (
-		<VehicleImageWrapper>
-			<Image src={`/images/vehicles/${src}.png`} {...restProps} alt={alt} />
-			<picture>
-				<source></source>
-			</picture>
-		</VehicleImageWrapper>
+		<Inner height={setHeight()}>
+			<ImageWrapper>
+				<VehicleImageWrapper>
+					<Image src={`/images/vehicles/${src}.png`} {...restProps} alt={alt} />
+					<picture>
+						<source></source>
+					</picture>
+				</VehicleImageWrapper>
+			</ImageWrapper>
+		</Inner>
 	);
 };
 
+export const VehicleName: React.FC = ({ children, ...restProps }) => {
+	return <Name {...restProps}>{children}</Name>;
+};
+
+export const VehicleGroup: React.FC = ({ children, ...restProps }) => {
+	return <Group {...restProps}>{children}</Group>;
+};
+
+export const VehicleDescription: React.FC = ({ children, ...restProps }) => {
+	return <Description {...restProps}>{children}</Description>;
+};
+
+export const VehicleLineup: React.FC = ({ children, ...restProps }) => {
+	return <Lineup {...restProps}>{children}</Lineup>;
+};
+
+export const VehicleLink: React.FC<LinkProps> = ({
+	to,
+	children,
+	...restProps
+}) => {
+	return (
+		<ButtonLink variant='yellow' to={to} {...restProps}>
+			{children}
+		</ButtonLink>
+	);
+};
+
+Vehicle.Description = VehicleDescription;
 Vehicle.Image = VehicleImage;
+Vehicle.Lineup = VehicleLineup;
+Vehicle.Name = VehicleName;
+Vehicle.Group = VehicleGroup;
+Vehicle.Link = VehicleLink;
 
 export default Vehicle;
--- spacex-graphql/src/components/Vehicle/styles/vehicle.ts
+++ spacex-graphql/src/components/Vehicle/styles/vehicle.ts
@@ -1,4 +1,9 @@
 import styled from 'styled-components/macro';
+import { StyledLinkButton } from 'styles/Button';
+
+export interface VehicleProps {
+	height?: string;
+}
 
 export const Container = styled.div`
 	display: block;
@@ -10,10 +15,11 @@ export const Container = styled.div`
 	}
 `;
 
-export const Inner = styled.div`
-	height: 400px;
+export const Inner = styled.div<VehicleProps>`
+	height: ${({ height }) => (height ? `${height}px` : '400px')};
+
 	@media (min-width: 768px) {
-		height: 800px;
+		height: ${({ height }) => (height ? `${height}px` : '800px')};
 	}
 `;
 
@@ -22,18 +28,18 @@ export const ImageWrapper = styled.div`
 	max-height: 850px;
 `;
 
-export const VehicleImageWrapper = styled.div`
+export const VehicleImageWrapper = styled.div<VehicleProps>`
 	position: relative;
 	overflow: hidden;
 	height: 100%;
 	max-height: 850px;
 `;
 
-export const Image = styled.img`
+export const Image = styled.img<VehicleProps>`
 	position: absolute;
 	top: 0;
 	left: 0;
-	height: 100%;
+	height: ${({ height }) => (height ? `${height}px` : '100%')};
 	object-position: center center;
 
 	max-height: 850px;
@@ -42,4 +48,47 @@ export const Image = styled.img`
 	object-fit: contain !important;
 `;
 
-export const Picture = styled.picture``;
+export const Name = styled.h3`
+	font-weight: 400;
+	margin: 0px 0px 2rem 0.5rem;
+	text-transform: uppercase;
+`;
+
+export const Group = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	gap: 4rem 1rem;
+	align-items: end;
+	padding: 3rem 0 0;
+
+	@media (min-width: 768px) {
+		grid-template-columns: repeat(4, 1fr);
+	}
+`;
+
+export const ButtonLink = styled(StyledLinkButton)`
+	margin-top: 1em;
+`;
+
+export const Description = styled.p`
+	color: #fff;
+	font-size: 1rem;
+	line-height: 1.6;
+	justify-self: center;
+	text-align: justify;
+	max-width: clamp(45ch, 50%, 75ch);
+`;
+
+export const Lineup = styled.div`
+	border-left: 1px dashed rgba(255, 255, 255, 0.25);
+	display: flex;
+	justify-content: space-between;
+	flex-flow: column nowrap;
+	text-align: center;
+	padding: 0px 0px 0px 0.5rem;
+	margin-bottom: 2rem;
+
+	@media (min-width: 992px) {
+		margin-bottom: 0;
+	}
+`;
