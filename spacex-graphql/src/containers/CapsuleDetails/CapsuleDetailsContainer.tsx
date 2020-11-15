3bc116d71f7b612849336ff4240eb7db5ef8a7db
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-15 20:41:34 +0100

    create containers

---
+++ spacex-graphql/src/containers/CapsuleDetails/CapsuleDetailsContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface CapsuleDetailsContainerProps {}
+
+const CapsuleDetailsContainer: React.SFC<CapsuleDetailsContainerProps> = () => {
+	return <div>capsule details container</div>;
+};
+
+export default CapsuleDetailsContainer;
---
+++ spacex-graphql/src/containers/DragonDetails/DragonDetailsContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface DragonDetailsContainerProps {}
+
+const DragonDetailsContainer: React.SFC<DragonDetailsContainerProps> = () => {
+	return <div>dragon details container</div>;
+};
+
+export default DragonDetailsContainer;
