233a491b7d39e7c43cb3979c51f3ce1211efbbc2
Joanna Chądzyńska <jmalek87@gmail.com>
2020-11-16 13:54:02 +0100

    update dragon details query

--- spacex-graphql/src/queries/dragonQueries/getDragonDetails.ts
+++ spacex-graphql/src/queries/dragonQueries/getDragonDetails.ts
@@ -3,24 +3,42 @@ import { gql } from 'apollo-boost';
 export const GET_DRAGON_DETAILS = gql`
 	query getDragonDetails($id: ID!) {
 		dragon(id: $id) {
+			active
 			crew_capacity
 			description
 			diameter {
 				feet
-				meters
 			}
-			dry_mass_kg
+			dry_mass_lb
 			first_flight
 			id
 			heat_shield {
 				material
 			}
+			height_w_trunk {
+				feet
+			}
 			name
 			launch_payload_mass {
-				kg
+				lb
 			}
 			return_payload_mass {
-				kg
+				lb
+			}
+			thrusters {
+				amount
+				fuel_1
+				fuel_2
+				pods
+				thrust {
+					lbf
+				}
+				type
+			}
+			trunk {
+				trunk_volume {
+					cubic_feet
+				}
 			}
 			type
 			wikipedia
