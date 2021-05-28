23597fdff180f8b4e7f754bfdd39ea70f311024c
Joanna Chądzyńska <jmalek87@gmail.com>
2021-05-28 21:13:37 +0200

    update rootReducer

--- spacex-graphql/src/app/rootReducer.ts
+++ spacex-graphql/src/app/rootReducer.ts
@@ -1,14 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import landpads from './../features/landpads/landpadsSlice';
-import launches from './../features/launches/launchesSlice';
-import launchpads from './../features/launchpads/launchpadsSlice';
-import rockets from './../features/rockets/rocketSlice';
+import landpads from 'features/landpads/landpadsSlice';
+import launches from 'features/launches/launchesSlice';
+import launchpads from 'features/launchpads/launchpadsSlice';
+import rockets from 'features/rockets/rocketSlice';
+import starlinks from 'features/starlinks/starlinksSlice';
 
 const rootReducer = combineReducers({
 	launches,
 	landpads,
 	launchpads,
 	rockets,
+	starlinks,
 });
 
 export default rootReducer;
