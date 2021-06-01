50d4ed90a2ba786d4c18ec7247c8ed12976bd57a
Joanna Chądzyńska <jmalek87@gmail.com>
2021-06-01 19:20:12 +0200

    fix header

--- spacex-graphql/src/containers/Card/components/Header.tsx
+++ spacex-graphql/src/containers/Card/components/Header.tsx
@@ -3,27 +3,23 @@ import React from 'react';
 import { FiCheckCircle, FiClock, FiXCircle } from 'react-icons/fi';
 import { Launch } from 'types/launchTypes';
 
-const Header: React.SFC<Launch> = ({ success, flight_number }) => {
+const Header: React.SFC<Launch> = ({ success, flight_number, upcoming }) => {
 	return (
 		<Card.Header launchSuccess={success}>
 			{success ? (
 				<span>
-					{success ? (
-						<>
-							<FiCheckCircle size='1.5em' />
-							<span>Successful</span>
-						</>
-					) : (
-						<>
-							<FiXCircle size='1.5em' />
-							<span>Failure</span>
-						</>
-					)}
+					<FiCheckCircle size='1.5em' />
+					<span>Successful</span>
 				</span>
-			) : (
+			) : upcoming ? (
 				<span>
 					<FiClock size='1.5em' />
 					<span>Upcoming</span>
+				</span>
+			) : (
+				<span>
+					<FiXCircle size='1.5em' />
+					<span>Failure</span>
 				</span>
 			)}
 
